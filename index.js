72ba986183f46d41858ab99f724098e9535c8746
CuentaBorrada <amateo1982@yahoo.es>
2020-06-14 23:13:41 +0200

    Últimos ajustes aplicados

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 /** Script JS */
+/** Global Variables */
 var loading     = false
 var beforeBytes = 0
 var afterBytes  = 0
 var percentage  = 0
 
+/** Onload Event */
 window.onload = function () {
-  var codeInput         = document.querySelector('.code-input')
-  var codeCompress      = document.querySelector('.code-compress')
-  var codeResultContent = document.querySelector('.code-result__content')
-  var copybutton        = document.querySelector('.copy-btn')
-  var resetButton       = document.querySelector('.reset-btn')
-  var compressionValue  = document.querySelector('.compression-value')
-  var compressionLevel  = document.querySelector('.compression-level')
-  var compressionIcons  = document.querySelector('.compression-icons')
+  /** HTML Elements */
+  var codeInput           = document.querySelector('.code-input')
+  var codeCompress        = document.querySelector('.code-compress')
+  var codeResultContent   = document.querySelector('.code-result__content')
+  var copybutton          = document.querySelector('.copy-btn')
+  var resetButton         = document.querySelector('.reset-btn')
+  var compressionValue    = document.querySelector('.compression-value')
+  var compressionLevel    = document.querySelector('.compression-level')
+  var compressionLevelBar = document.querySelector('.compression-level__bar')
+  var compressionIcons    = document.querySelector('.compression-icons')
 
   codeInput.focus()
 
+  /** Events */
   copybutton.onclick = function () {
     copyToClipboard(codeResultContent)
   }
@@ -43,60 +48,69 @@ window.onload = function () {
     checkLoading(loading)
 
     setTimeout(function () {
-      var compressionLevelBar = document.querySelector('.compression-level__bar')
-
       compressCss(codeInput.value).then(function (result) {
-        codeResultContent.innerHTML = result
-
         loading = false
         checkLoading(loading)
 
+        codeResultContent.innerHTML = result
+
         compressionValue.classList.add('show')
         compressionValue.innerHTML = '<span class="compression-value__percentage">' + percentage + '%</span> de compresión | ' + beforeBytes + ' > ' + afterBytes + ' (bytes)'
-
         compressionLevel.classList.add('show')
         compressionLevelBar.style.width = percentage + '%'
-
         compressionIcons.classList.add('show')
 
         codeInput.value = ''
         codeInput.focus()
       })
-    }, 1000)
+    }, 500)
   }
+}
+
+/** Functions */
+function firstPartCompression (code) {
+  var localResult = ''
+  var controlPoint = '}'
+  
+  for(var i = 0; i < code.length; i++) {
+    if (
+      code[i] === '{' ||
+      code[i] === '}' ||
+      code[i] === ':' ||
+      code[i] === ';'
+    ) {
+      controlPoint = code[i]
+    }
+    if (
+      code[i].length === 1 &&
+      code[i].match(/[a-z0-9\{\}\:\;\#\.\[\]\^\~\+\/\*\(\)\,\>\-]/i) ||
+      ((controlPoint === '}' && code[i] === ' ') || controlPoint === ':')
+    ) {
+      localResult += code[i]
+    }
+  }
+
+  return localResult
+}
+
+function secondPartCompression (result) {
+  result = result.replace(/ {/g, '{')
+  result = result.replace(/: /g, ':')
+  result = result.replace(/;}/g, '}')
+  result = result.replace(/0\./g, '.')
+  result = result.replace(/ > /g, '>')
+  result = result.replace(/ \+ /g, '+')
+  result = result.replace(/ ~ /g, '~')
+  result = result.replace(/, /g, ',')
+
+  return result
 }
 
 function compressCss (code) {
   return new Promise(function (resolve, reject) {
-    var result = ''
-    var controlPoint = '}'
-    
-    for(var i = 0; i < code.length; i++) {
-      if (
-        code[i] === '{' ||
-        code[i] === '}' ||
-        code[i] === ':' ||
-        code[i] === ';'
-      ) {
-        controlPoint = code[i]
-      }
-      if (
-        code[i].length === 1 &&
-        code[i].match(/[a-z0-9\{\}\:\;\#\.\[\]\^\~\+\/\*\(\)\,\>\-]/i) ||
-        ((controlPoint === '}' && code[i] === ' ') || controlPoint === ':')
-      ) {
-        result += code[i]
-      }
-    }
-
-    result = result.replace(/ {/g, '{')
-    result = result.replace(/: /g, ':')
-    result = result.replace(/;}/g, '}')
-    result = result.replace(/0\./g, '.')
-    result = result.replace(/ > /g, '>')
-    result = result.replace(/ \+ /g, '+')
-    result = result.replace(/ ~ /g, '~')
-    result = result.replace(/, /g, ',')
+    var
+    result = firstPartCompression(code)
+    result = secondPartCompression(result)
 
     beforeBytes = code.length
     afterBytes = result.length
